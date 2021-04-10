90241ca43e2ec64697809e4e8533f70212653c76
Ronald Velásquez <edwinvelasquez@bcp.com.pe>
2021-04-09 19:25:52 -0500

    First commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+h
+8app/src/main/java/com/example/pokedex/PokedexActivity.kt,1/e/1ea583a21bbd31d592c16c83efbd825164d7c1f3
---
+++ app/src/main/java/com/example/pokedex/PokedexActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pokedex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PokedexActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pokedex)
+    }
+}
