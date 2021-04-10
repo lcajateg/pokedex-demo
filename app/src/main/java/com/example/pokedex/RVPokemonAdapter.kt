7c9c6c97321f8087471df15d03502144f1a2576a
Ronald Velásquez <edwinvelasquez@bcp.com.pe>
2021-04-09 20:39:14 -0500

    Add adapter

---
+++ app/src/main/java/com/example/pokedex/RVPokemonAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.pokedex
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pokedex.model.Pokemon
+
+class RVPokemonAdapter(
+    private val pokemonList: MutableList<Pokemon>
+) : RecyclerView.Adapter<RVPokemonAdapter.PokemonViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_pokemon, parent, false)
+        return PokemonViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: PokemonViewHolder, position: Int) {
+        val pokemon = pokemonList[position]
+        holder.bind(pokemon)
+    }
+
+    override fun getItemCount() = pokemonList.size
+
+    class PokemonViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(pokemon: Pokemon) {
+            val txtPokemonName = itemView.findViewById<TextView>(R.id.txt_pokemon_name)
+            txtPokemonName.text = pokemon.name
+        }
+    }
+}
---
+++ app/src/main/java/com/example/pokedex/model/Pokemon.kt
@@ -0,0 +1,3 @@
+package com.example.pokedex.model
+
+data class Pokemon(val name: String, val url: String)
